4c84023107b811eb3877b7c9c9603bc231442b5e
halfclub58 <sonkesriyas@gmail.com>
2025-11-14 14:21:50 +0530

    Add local changes for rmx1971 kernel with Nethunter support

---
+++ nethunter/out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Nov 14 14:12:19 IST 2025"
+#define LINUX_COMPILE_BY "halfclub"
+#define LINUX_COMPILE_HOST "dell"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
